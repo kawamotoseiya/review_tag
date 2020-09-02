9f75c9ff12d7a735d356b9a8e9bca3d5310d1d96
vagrant <vagrant@localhost.localdomain>
2020-09-02 16:22:42 +0000

    star

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,6 @@ class BooksController < ApplicationController
 	end
 	private
 	def book_params
-		params.require(:book).permit(:title, :body, :tag_list)
+		params.require(:book).permit(:title, :body, :tag_list, :rate)
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_02_101834) do
+ActiveRecord::Schema.define(version: 2020_09_02_152408) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rate"
   end
 
   create_table "taggings", force: :cascade do |t|
